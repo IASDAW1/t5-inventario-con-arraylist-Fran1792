af41416c46c5e30e668789787c5b92c91595239e
Fran <Fran@DESKTOP-MF6KK54>
2024-01-12 12:08:59 +0100

    commit 1 practica realizada

---
+++ a1inventario/src/main/java/com/mycompany/a1inventario/A1inventario.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.a1inventario;
+
+/**
+ *
+ * @author Fran
+ */
+public class A1inventario {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("solucion");
+    }
+}
