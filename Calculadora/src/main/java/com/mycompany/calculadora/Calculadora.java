9a7ea6a9de2cd98e9cd733573850a8fe88c1a6f3
Fran <Fran@DESKTOP-MF6KK54>
2024-03-06 17:06:58 +0100

    commit 1 practica realizada

---
+++ Calculadora/src/main/java/com/mycompany/calculadora/Calculadora.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadora;
+
+/**
+ *
+ * @author Fran
+ */
+/**
+ * Esta clase representa una calculadora simple que puede sumar dos números.
+ */
+public class Calculadora {
+    
+public static void main(String[] args) {
+    
+}
+
+    /**
+     * Este método suma dos números enteros.
+     * @param a El primer número a sumar.
+     * @param b El segundo número a sumar.
+     * @return La suma de los dos números.
+     */
+    
+
+public int sumar(int a, int b) {
+        return a + b;
+    }
+}
+
